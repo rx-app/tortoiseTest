e44acf8c47f0b16a39831edab24674d2b0d91c5b
rxapp <rxapp@yandex.com>
2020-03-30 19:26:54 +0800

    no message

--- 2.js
+++ 2.js
@@ -1,7 +1,8 @@
 let a=1234;
 console.log(a)
 console.log(a)
-console.log(a)
+let b=1;
+console.log(abc)
 console.log(a)
 console.log(a)
 console.log(a)
--- 3.js
+++ 3.js
@@ -1,13 +1,10 @@
 let a=2
 let a=2
 let a=2
+
 let a=2
 let a=2
-let a=2
-let a=2
-let a=2
-let a=2
-let a=2
-let a=2
-let a=2
+let aaa=2323
+let a=3
+let aaaa=2
 let a=2
